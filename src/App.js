b93ca4a0dd8c527e32a8168c7dad6b89eb3d4869
ManasTripathi-dev <manas30july@gmail.com>
2021-03-03 20:31:26 +0530

    mod 3 solution

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css'
 import Quiz from './Quiz.js'
 
 
+
+
 class App extends Component {
   render() {
     return (
@@ -12,6 +14,8 @@ class App extends Component {
     )
   }
 }
+
+
 
 
 export default App
