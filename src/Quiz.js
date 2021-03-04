6d71248acd1aedfc0313d75d778d0abfa0ac0ec2
ManasTripathi-dev <manas30july@gmail.com>
2021-03-04 11:00:02 +0530

    mod 4 completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,16 +1,44 @@
 
-
-import React, {Component} from 'react'
+import React, { Component }        from 'react'
 import QuizQuestion from './QuizQuestion.js'
-let quizData=require('./quiz_data.json')
-class Quiz extends Component{
+import QuizEnd from './QuizEnd.js'
+
+
+
+let quizData = require('./quiz_data.json')
+
+
+
+
+class Quiz extends Component {
+
+
+
+
 constructor(props){
-super(props);
-this.state= {quiz_position:1};
+ super(props);
+ this.state = {
+   quiz_position: 1
+ }       ;
 }
-render(){
-return(
-<QuizQuestion quiz_question={quizData.quiz_questions[0]}/>
+
+
+
+
+render() {
+
+
+const isQuizEnd =  ( (this.state.quiz_position - 1) === quizData.quiz_questions.length ) ? true : false
+return (
+
+
+  <div>
+  {        isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question = {       quizData.quiz_questions[this.state.quiz_position-1]}         /> }
+
+
+  </div>
+
+
 );
 }
 }
